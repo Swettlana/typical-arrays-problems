b2edc1e7cc5e55ca9279cf4e28776f02f996d754
Swettlana <stellla@tut.by>
2021-03-15 13:39:20 +0300

    feat: add new task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((arguments.length===0)||(array.length===0)){
+    return 0;
+  }
+    else{
+     let min=array[0];
+     array.forEach(a=> {
+       if (a<min) {
+        min=a;
+    }});
+  return min;
+    }
 }
+
 
 exports.max = function max (array) {
-  return 0;
-}
+  if ((arguments.length===0)||(array.length===0)){
+    return 0;
+  }
+    else{
+     let max=array[0];
+     array.forEach(a=> {
+       if (a>max) {
+        max=a;
+    }});
+  return max;
+    }
+ }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if ((arguments.length===0)||(array.length===0)){
+    return 0;
+  }
+    else{
+  let summ=array.reduce((sum,array)=>sum+array,0);
+  return summ/array.length;
+    }
+ }
